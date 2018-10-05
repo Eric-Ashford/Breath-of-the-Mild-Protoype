07538ce5f06083f21d43524f2578c90655301dee
Eric Ashford <eri.ashford@gmail.com>
2018-10-04 22:09:52 -0500

    Updated Base Game

---
+++ CapTaffy/Assets/Scripts/Controller/InputHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+	private float vertical;
+	private float horizontal;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		
+	   GetInput();
+
+	}
+
+
+	void GetInput()
+	{
+		vertical = Input.GetAxis("Vertical");
+		horizontal = Input.GetAxis("Horizontal");
+	}
+}
---
+++ CapTaffy/Assets/Scripts/cameraController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class cameraController : MonoBehaviour
+{
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 2;
+
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+
+    public float rotationSmoothTime = .05f;
+    private Vector3 rotationSmoothVelocity;
+    private Vector3 currentRotation;
+
+    private float yaw;
+    private float pitch;
+
+    public bool lockCursor;
+
+
+    void Start()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+
+        //Vector3 targetRotation = new Vector3(pitch, yaw);
+        transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+    }
+}

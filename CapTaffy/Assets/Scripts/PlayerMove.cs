778a700db01dc6c41d306d46729d2fb3f2a47aca
Caleb Flick <calebflick@gmail.com>
2018-10-05 07:27:30 -0500

    added footstep sfx code

--- CapTaffy/Assets/Scripts/PlayerMove.cs
+++ CapTaffy/Assets/Scripts/PlayerMove.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
-//[RequireComponent(typeof(AudioSource))]
+[RequireComponent(typeof(AudioSource))]
 
 public class PlayerMove : MonoBehaviour
 {
@@ -21,9 +21,12 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     PhysicMaterial maxFriction;
 
+    [SerializeField]
+    AudioClip[] footstepsArray;
+
     Rigidbody rb;
     CapsuleCollider cc;
-    //AudioSource footstep;
+    AudioSource footstep;
     Transform cameraTransform;
 
     Vector3 facingDirection;
@@ -36,6 +39,7 @@ public class PlayerMove : MonoBehaviour
     bool isRunning;
     bool isJumping;
     bool isOnGround;
+    bool isTakingStep;
 
     const string horizontalAxisName = "Horizontal";
     const string verticalAxisName = "Vertical";
@@ -46,13 +50,14 @@ public class PlayerMove : MonoBehaviour
         isRunning = false;
         isJumping = false;
         isOnGround = true;
+        isTakingStep = false;
     }
 
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody>();
         cc = this.gameObject.GetComponent<CapsuleCollider>();
-        //footstep = this.gameObject.GetComponent<AudioSource>();
+        footstep = this.gameObject.GetComponent<AudioSource>();
         cameraTransform = Camera.main.transform;
     }
 
@@ -64,7 +69,7 @@ public class PlayerMove : MonoBehaviour
     void FixedUpdate()
     {
         MovePlayer();
-        //PlayFootstep();
+        PlayFootstep();
     }
 
     void ChangeFrictionMaterial()
@@ -135,26 +140,42 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    // TODO: change to use footstep array code
-    //void PlayFootstep()
-    //{
-    //    if (rb.velocity.magnitude > 4.0f && !footstep.isPlaying)
-    //    {
-    //        // placeholder footstep
-    //        // TODO: need to tweak randomization/walk speed
+    void PlayFootstep()
+    {
+        if (rb.velocity.magnitude > 2.0f && !footstep.isPlaying && !isTakingStep && isOnGround)
+        {
+            // choose random clip, excluding the first one
+            int n = Random.Range(1, footstepsArray.Length - 1);
+            footstep.clip = footstepsArray[n];
 
-    //        // randomizes volume and pitch for every step, increasing with walk speed
-    //        footstep.volume = Mathf.Clamp(Random.Range(0.55f, 0.75f) * rb.velocity.magnitude, 0.0f, 1.0f);
-    //        footstep.pitch = Random.Range(0.85f, 1.15f) * (rb.velocity.magnitude / 6);
+            // randomizes volume and pitch for every step, increasing with walk speed
+            footstep.volume = Mathf.Clamp(Random.Range(0.4f, 0.65f) * rb.velocity.magnitude, 0.0f, 1.0f);
+            footstep.pitch = Random.Range(0.9f, 1.1f) * (Mathf.Clamp(rb.velocity.magnitude / 7, 1.0f, 1.5f));
 
-    //        footstep.Play();
-    //    }
-    //}
+            footstep.Play();
+
+            // move clip to first index so it won't play again right away
+            footstepsArray[n] = footstepsArray[0];
+            footstepsArray[0] = footstep.clip;
+
+            StartCoroutine(TakeStep());
+        }
+    }
+
+    IEnumerator TakeStep()
+    {
+        isTakingStep = true;
+
+        // pause in between playing footstep sound, decreasing with walk speed
+        yield return new WaitForSecondsRealtime(0.5f / (Mathf.Clamp(rb.velocity.magnitude / 7, 0.8f, 1.5f)));
+
+        isTakingStep = false;
+    }
 
     void OnCollisionEnter(Collision other)
     {
         //check if on the ground
-        if (other.gameObject.tag == "Ground")       //need to use ground tag for any jumpable surface
+        if (other.gameObject.tag == "Ground")       //need to use ground tag for any walkable surface
         {
             isOnGround = true;
             rb.drag = 5;        //increase drag when on ground

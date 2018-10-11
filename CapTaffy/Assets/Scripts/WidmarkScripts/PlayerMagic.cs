a94e75c59dff7f9d356c8b273e441ae31f9553e1
Widmark Navas-Ayala <widmark.navasayala@loop.colum.edu>
2018-10-11 13:25:03 -0500

    Reimport

---
+++ CapTaffy/Assets/Scripts/WidmarkScripts/PlayerMagic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMagic : MonoBehaviour
+{
+    [SerializeField]
+    Slider magicBar;
+
+    public float currentMagic;
+    const int maxMagic = 100;
+    bool coolDownHasStarted;
+
+    void Start ()
+    {
+        coolDownHasStarted = false;
+        currentMagic = maxMagic;
+	}
+	
+	
+	void Update ()
+    {
+        CastMagic();
+        UpdateMagicBar();
+        MagicBarCoolDown();
+        MagicBarHasCooledDown();
+    }
+
+    void CastMagic()
+    {
+       if (Input.GetKey(KeyCode.M) && currentMagic == maxMagic)
+        {
+            currentMagic = 0f;
+        }
+    }
+
+    void UpdateMagicBar()
+    {
+        magicBar.value = currentMagic / maxMagic;
+    }
+
+    void MagicBarCoolDown()
+    {
+        if (currentMagic < maxMagic)
+        {
+            coolDownHasStarted = true;
+            currentMagic += Time.deltaTime * 50;
+        }
+    }
+
+    void MagicBarHasCooledDown()
+    {
+        if (coolDownHasStarted = true && currentMagic >= maxMagic)
+        {
+            coolDownHasStarted = false;
+            currentMagic = 100;
+        }
+    }
+}

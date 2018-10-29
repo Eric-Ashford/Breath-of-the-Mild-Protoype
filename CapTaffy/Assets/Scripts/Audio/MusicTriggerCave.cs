0243aea6a63e9c883c4f19c5c0e944cea1f4d17c
crosbyn <32074054+crosbyn@users.noreply.github.com>
2018-10-28 19:55:20 -0500

    Added Damage Sound to Beetle and Music with a trigger system set up

---
+++ CapTaffy/Assets/Scripts/Audio/MusicTriggerCave.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTriggerCave : MonoBehaviour {
+
+    [FMODUnity.EventRef]
+    public string musicTriggerCave;
+
+    FMOD.Studio.EventInstance CaveMusicEvent;
+
+    private void Start()
+    {
+        CaveMusicEvent = FMODUnity.RuntimeManager.CreateInstance(musicTriggerCave);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        CaveMusicEvent.start();
+    }
+}
--- CapTaffy/Assets/Scripts/WidmarkScripts/DamageEnemy.cs
+++ CapTaffy/Assets/Scripts/WidmarkScripts/DamageEnemy.cs
@@ -8,13 +8,17 @@ public class DamageEnemy : MonoBehaviour
     [SerializeField]
     private float attackDamage = 15f;
 
+    [FMODUnity.EventRef]
+    public string enemyDamageSound;
+
     void OnTriggerEnter(Collider other)
     {
 
         if (other.tag == "Enemy")
         {
-            other.gameObject.GetComponent<EnemyHealth>().DamageEnemy(attackDamage); // player takes damage
+            other.gameObject.GetComponent<EnemyHealth>().DamageEnemy(attackDamage); // enemy takes damage
 
+            FMODUnity.RuntimeManager.PlayOneShot(enemyDamageSound);
             //this.gameObject.SetActive(false);
         }
     }
